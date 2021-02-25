99875d7e08c7225b2532142fc413e8edebd3af14
Helena Copppieters <hcoppieters@hotmail.com>
2021-02-25 15:29:58 +0100

    Oplossing oefening CompanyManagerController met Index Methode en Razor view

---
+++ CompanyEmployees.MVC/Controllers/CompanyManagerController.cs
@@ -0,0 +1,25 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.MVC.Controllers
+{
+    public class CompanyManagerController : Controller
+    {
+        
+        private IRepositoryManager _repositoryManager;
+
+        public CompanyManagerController(IRepositoryManager repositoryManager)
+        {
+            _repositoryManager = repositoryManager;
+        }
+        public IActionResult Index()
+        {
+            var companies = _repositoryManager.Company.GetAllCompanies(false);
+            return View(companies);
+        }
+    }
+}
