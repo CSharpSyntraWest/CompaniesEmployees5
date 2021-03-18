e45cd8c20da3dd309178a4a1afb20121730c96aa
Helena Copppieters <hcoppieters@hotmail.com>
2021-03-18 10:17:04 +0100

    Opgave oefening Index methode testen van de CompanyManagerController

---
+++ CompanyEmployees.MVC.Test/CompanyManagerControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using Contracts;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.MVC.Test
+{
+    public class CompanyManagerControllerTests
+    {
+        private Mock<IRepositoryManager> mockRepo;
+        [SetUp]
+        public void Initialize()
+        {
+            mockRepo = new Mock<IRepositoryManager>();
+            mockRepo.Setup(repo => repo.Company.GetAllCompanies(false))
+                .Returns(SeedTestData.GetTestCompanies());
+        }
+        [Test]
+        public void Index_ReturnsAViewResult_WithListOfCompanies()
+        { 
+            //Oefening: maak aan het voorbeeld van de EmployeeMangerControllerTest
+            //de test methode voor Index van CompanyManagerController
+        }
+    }
+}
---
+++ CompanyEmployees.MVC.Test/EmployeeManagerControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using CompanyEmployees.MVC.Controllers;
+using Contracts;
+using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.MVC.Test
+{
+    public class EmployeeManagerControllerTests
+    {
+        private Mock<IRepositoryManager> mockRepo;
+        [SetUp]
+        public void Initialize()
+        {
+            mockRepo = new Mock<IRepositoryManager>();
+            mockRepo.Setup(repo => repo.Company.GetAllCompanies(false))
+                .Returns(SeedTestData.GetTestCompanies());
+        }
+        [Test]
+        public void Index_ReturnsAViewResult_WithListOfEmployees()
+        {
+            //Arrange
+            mockRepo.Setup(repo => repo.Employee.GetAllEmployees(false))
+                .Returns(SeedTestData.GetTestEmployees());
+            var controller = new EmployeeManagerController(mockRepo.Object);
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsInstanceOf<ViewResult>(result);
+            ViewResult viewResult = result as ViewResult; //casting, kan ook via (ViewResult)result
+            Assert.IsAssignableFrom<List<Employee>>(viewResult.ViewData.Model);
+            List<Employee> model = viewResult.ViewData.Model as List<Employee>;
+            Assert.AreEqual(2, model.Count());
+        }
+    }
+}
